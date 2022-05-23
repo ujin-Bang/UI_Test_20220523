0273ba83d5e686d41e0c37a723d1df16ec27a388
ujin-Bang <qkddnwls14@gmail.com>
2022-05-23 23:00:01 +0900

    Initial commit

---
+++ app/src/main/java/com/start/ui_test_20220523/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.start.ui_test_20220523
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
